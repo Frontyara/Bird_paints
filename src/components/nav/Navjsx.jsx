325ecbd44fa1e0b7e05ba154b3f4e4b64103c33e
Autor <drop1for1me@gmail.com>
2024-11-14 22:37:11 +0500

    add visual burger-menu

--- src/components/nav/Navjsx.jsx
+++ src/components/nav/Navjsx.jsx
@@ -1,11 +1,19 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ReduxStateContext } from "../../main"
 import { NavLink } from "react-router-dom"
 
 export default function Nav() {
+    const [boolean, reverseBoolean] = useState(false)
+    function reverseBool() {
+        reverseBoolean(!boolean)
+        if (boolean == true) {
+            // document.querySelectorAll('.item')
+        }
+    }
     const context = useContext(ReduxStateContext)
     const contextItem = context.nav.item
     let contextItemLogo = context.nav.itemLogo
+    console.log(boolean)
 
     return (
         <div className="nav">
@@ -13,13 +21,20 @@ export default function Nav() {
                 <div className="logo"><span>Ink. House</span></div>
                 <div className="line"></div>
                 <ul className="nav__items">
-                    {contextItem.map((item, index) => {
-                        return (
-                            <li className="item">
-                                <NavLink to={item.to} key={index} className="navLink"><span>{item.text}</span></NavLink>
-                            </li>
-                        )
-                    })}
+                    <div className="item__burger" onClick={reverseBool}>
+                        <div className="item__line"></div>
+                        <div className="item__line"></div>
+                        <div className="item__line"></div>
+                    </div>
+                    <li className="item__menu">
+                        {contextItem.map((item, index) => {
+                            return (
+                                <li className="item">
+                                    <NavLink to={item.to} key={index} className="navLink"><span>{item.text}</span></NavLink>
+                                </li>
+                            )
+                        })}
+                    </li>
                     {contextItemLogo.map((item, index) => {
                         return (
                             <li>
